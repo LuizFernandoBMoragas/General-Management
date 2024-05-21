2193b006ab1927055b72fd810cf22dc99af19c2f
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-05-21 23:53:24 +0300

    new conection to database using sequelize ORM

--- server.js
+++ server.js
@@ -9,14 +9,21 @@ app.get('/', (req,res)=>{
     res.status(200).json({
         status: 'success',
         message: 'It is working and running'
-    })
+    });
 });
 
 app.use('/api/v1/auth', authRouter);
 
+app.use('*', (req,res, next)=>{
+    res.status(404).json({
+        status: 'fail',
+        message: 'route not found',
+    });
+});
 
-const port = process.env.APP_PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server is up and running on ${port}`);
+const PORT = process.env.APP_PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running on ${PORT}`);
 });
