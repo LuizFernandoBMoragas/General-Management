bec9dbb3d036add76407ea562b7113063275f055
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-05-27 04:13:37 +0300

    JWT and encrypting password

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,13 @@
+require('dotenv').config();
+
 const user = require('../db/models/user');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (payload)=>{
+    return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    })
+}
 
 const signup = async (req, res, next) => {
     const body = req.body;
@@ -16,9 +25,19 @@ const signup = async (req, res, next) => {
         lastName: body.lastName,
         email: body.email,
         password: body.password,
+        confirmPassword: body.confirmPassword,
+    });
+
+    const result = newUser.toJSON();
+
+    delete result.password;
+    delete result.deletedAt;
+
+    result.token = generateToken({
+        id: result.id
     });
     
-    if(!newUser){
+    if(!result){
         return res.status(400).json({
             status: 'fail',
             message: 'fail to create the user',
@@ -27,7 +46,7 @@ const signup = async (req, res, next) => {
 
     return res.status(201).json({
         status: 'success',
-        data: newUser,
+        data: result,
     });
 
 };
